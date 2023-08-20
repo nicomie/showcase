c86d730acb4dcfa1967b00c45c3abc3c609cca3f
nicomie <editsbymystic@gmail.com>
2023-08-20 12:08:40 +0200

    click logo

--- src/components/Wave.vue
+++ src/components/Wave.vue
@@ -5,18 +5,34 @@
 </script>
 
 <template>
-    <section class="flex h-[600px] w-screen absolute bottom-0 bg-secondary justify-center">
-    <div class="flex  h-[600px] ] ">
+
+    <section class="flex flex-col items-center h-[100%] w-screen absolute bottom-0 bg-secondary ">
+
+        <div class="w-screen relative bg-secondary_contrast mt-0 grow-0 " > 
+
+            <img src="../assets/go.png" alt="" class="w-24 h-24 m-auto " >
+        </div>
+
+
+    <div class="flex">
         
-        <div class="container m-auto grid grid-cols-[350px_450px] gap-3  [&>*]:rounded-md">
-            <div><CardS title="hej" ></CardS></div>
-            <div class="row-span-2 h-[460px]"><CardL title="Hello"></CardL></div>
-            <div><CardS title="då"></CardS></div>
+     
+        <div class=" mt-20  grid grid-cols-[350px_800px] gap-3  [&>*]:rounded-md ">
+                 
+            <CardS title="hej" :tags="list" class="" ></CardS>
+           
+           
+            <div class="row-span-2 ">
+                
+            <CardL title="Café ordering backend" :tags="list"></CardL>
+            </div>
+            <CardS title="då" :tags="list" class=""></CardS>
+            
         </div>
     </div>
+
 </section>
 </template>
 
 <style scoped>
-
 </style>
