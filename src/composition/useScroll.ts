0d05547bbc664c9ac13f277448ad22eb5e4ba4e6
nicomie <editsbymystic@gmail.com>
2023-08-22 16:54:10 +0200

    new composable scroll event

---
+++ src/composition/useScroll.ts
@@ -0,0 +1,48 @@
+import {Ref, onMounted, onUnmounted, ref} from 'vue'
+
+export function useScroll(element: Ref<HTMLElement> | Ref<null> , duration: number) {
+
+    const isBlocked = ref(false)
+    const direction = ref(false)
+    const triggered = ref(false)
+
+    const handle = (e: any) => {
+        if(!isBlocked.value){
+            isBlocked.value = true;
+
+            const scrollEvent = e as any
+       
+            triggered.value = true
+            if(scrollEvent.deltaY > 0){
+                direction.value = false
+            } else {
+                direction.value = true
+            }
+
+            setTimeout(() => {
+
+                isBlocked.value = false
+                triggered.value = false
+
+            }, duration)
+        }
+    }
+
+ 
+    onMounted(() => {
+        if(element.value) {
+            element.value.addEventListener("wheel", handle)
+        }
+   
+    })
+    onUnmounted(() => {
+        if(element.value) {
+            element.value.removeEventListener("wheel", handle)
+        }
+    })
+
+    return {
+        direction,
+        triggered
+    }
+}
