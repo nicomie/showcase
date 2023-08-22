7bc30f54afca97bcf26315e5c0af196e63ee6236
nicomie <editsbymystic@gmail.com>
2023-08-22 16:54:03 +0200

    cch

--- src/composition/useMouse.ts
+++ src/composition/useMouse.ts
@@ -1,24 +1,20 @@
-import {ref, onMounted, onBeforeUnmount} from 'vue'
+import {ref} from 'vue'
+
 
 export function useMouse() {
     const mouseX = ref(0)
     const mouseY = ref(0)
 
-    const update = (e: MouseEvent) => {
-        mouseX.value = e.pageX;
-        mouseY.value = e.pageY;
+    const update = (e: Event) => {
+        const ex = e as MouseEvent
+        mouseX.value = ex.pageX;
+        mouseY.value = ex.pageY;
     };
 
-    onMounted(() => {
-        window.addEventListener('mousemove', update)
-    })
 
-    onBeforeUnmount(() => {
-        window.removeEventListener('mousemove', update)
-    })
 
     return {
         mouseX,
         mouseY,
-      };
+    };
 }
