2f56ddd452b028f51353bf7eef8b5ad90a670268
nicomie <editsbymystic@gmail.com>
2023-08-24 12:10:36 +0200

    added project store

---
+++ src/views/Project.vue
@@ -0,0 +1,22 @@
+<template>
+    <section class="w-screen h-screen bg-main text-white flex items-center justify-center flex-col">
+    <h1 class="text-5xl">{{ data.name }}</h1>
+    <ul class="mt-5 max-w-[95%] ">
+        <li class="text-xl" v-for="(tag, index) in data.tags" :key="index">
+           <p>{{ tag }}</p> 
+        </li>
+    </ul>
+    </section>
+
+</template>
+
+<script setup lang="ts">
+import { useRoute } from 'vue-router';
+import projects from '../store/data.ts'
+
+    const route = useRoute()
+
+    let id = parseInt(route.params.id[0])
+
+    let data = projects.projects[id]
+</script>
--- src/views/Work.vue
+++ src/views/Work.vue
@@ -1,6 +1,20 @@
 <template>
+    <section class="work bg-main h-screen text-white flex items-center justify-center">
+       <div class="grid grid-cols-[350px_350px_350px] gap-10 "
+>
 
+            <Card v-for="(card, index) in projects.projects" :key="index" :title="card.name" :tags="card.tags" :index="index" >
+   
+              
+            </Card>
+        </div>
+    </section>
 </template>
 
 <script setup lang="ts">
+
+
+import Card from '../components/showcase/Card.vue';
+import projects from '../store/data.ts'
+
 </script>
