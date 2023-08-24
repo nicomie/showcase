4e5924ec39bff4c1bf6506c16ad77918483c0d6b
nicomie <editsbymystic@gmail.com>
2023-08-24 12:48:08 +0200

    aa

--- src/components/showcase/Wrapper.vue
+++ src/components/showcase/Wrapper.vue
@@ -8,7 +8,7 @@
       <article class="">
         <CardL title="Recipe creation service" img="src/assets/food.png"></CardL>
       </article>
-      <p class=" absolute text-white text-3xl bottom-0 whitespace-nowrap left-0 translate-y-[300px] gap-5 items-center flex">View all my work <img class="h-8 w-12" src="@/assets/arrow_straight.svg"/></p>
+      <p class=" absolute text-white text-3xl bottom-0 whitespace-nowrap left-0 translate-y-[300px] gap-5 items-center flex">View all my work <img class="h-8 w-12" src="../../assets/arrow_straight.svg"/></p>
     </section>
  
   </div>
