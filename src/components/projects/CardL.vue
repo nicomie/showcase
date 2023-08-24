1991094613edd20275f44f90c860cbd49f3b8e21
nicomie <editsbymystic@gmail.com>
2023-08-24 13:45:16 +0200

    images!

--- src/components/projects/CardL.vue
+++ src/components/projects/CardL.vue
@@ -1,17 +1,22 @@
 <template>
     <section class="flex flex-col h-full w-full gap-5 relative">
         <p class="after:content-[''] after:w-[33%] after:h-1 after:mt-3 after:bg-secondary_pop after:block text-white text-5xl ">{{title}}</p>
-        <img :src="img" alt="" class="h-[100%] w-[100%] m-auto">
+        <img :src="imageUrl"  alt="" class="h-[100%] w-[100%] m-auto">
         
 </section>
 
 </template>
 
 <script setup lang="ts">
-    defineProps({
+
+    const props = defineProps({
         title: String,
         tags: Array<String>,
         img: String
     })
 
+    const imageUrl = new URL(`../../assets/${props.img}`, import.meta.url).href
+
+
+ 
 </script>
--- src/components/showcase/Wrapper.vue
+++ src/components/showcase/Wrapper.vue
@@ -3,11 +3,11 @@
   <div class="h-screen w-screen flex items-center justify-center " :style="{'background-color': color }">
     <section class="flex gap-32 [&>*]:h-[300px] [&>*]:w-[450px] [&>*]:rounded  relative">
       <article class="">
-        <CardL title="Café ordering system" img="../../assets/i.svg"></CardL>
+        <CardL title="Café ordering system" img="i.svg"></CardL>
 
       </article>
       <article class="">
-        <CardL title="Recipe creation service" img="../../assets/food.png"></CardL>
+        <CardL title="Recipe creation service" img="food.png"></CardL>
       </article>
       <p class=" absolute text-white text-3xl bottom-0 whitespace-nowrap left-0 translate-y-[300px] gap-5 items-center flex">View all my work <img class="h-8 w-12" src="../../assets/arrow_straight.svg"/></p>
     </section>
