5f2fb609fb9d7788167ed8130d5d4e1a53958693
nicomie <editsbymystic@gmail.com>
2023-08-24 13:52:32 +0200

    header

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-
+  import Header from './components/header/Header.vue';
 </script>
 
 <template >
-
+  <Header></Header>
   <RouterView></RouterView>
 </template>
 
--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -1,8 +1,5 @@
 <template>
-    <nav class="absolute top-0 h-24 w-screen bg-secondary_contrast flex content-center justify-around ">
-        <ul class="[&>li]:text-xl flex items-center gap-20">
-          <li>Nicholas Miettinen</li>
-          <li>Hej</li>
-        </ul>
-      </nav>
+    <nav class="absolute top-0 h-24 w-screen bg-secondary_contrast flex items-center justify-around z-10">
+      <p class="text-5xl text-white">Site is under construction 🚧</p>  
+    </nav>
 </template>
