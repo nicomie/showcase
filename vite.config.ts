084a447eab97b10503ed40d6003b2c2e35d77f41
nicomie <editsbymystic@gmail.com>
2023-08-24 12:45:32 +0200

    a

--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,5 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   base: '/showcase/',
   plugins: [vue()],
-  resolve: {
-    alias: [
-      { find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url)) },
-    ]
-  }
+
 })
